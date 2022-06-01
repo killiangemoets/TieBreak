873c0ef49b8f0e789f520be577f7a097e0f62e56
Killian Gemoets <kilgem@live.fr>
2022-06-01 08:54:41 +0200

    testheroku2

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -18,87 +18,37 @@ import ClubProfile from "./screens/ClubProfile";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // IMPORT REDUX //
-import { createStore, combineReducers } from "redux";
-import { Provider } from "react-redux";
-import token from "./reducer/token";
-import currentReservation from "./reducer/currentReservation";
-
-const store = createStore(combineReducers({ token, currentReservation }));
+// import { createStore, combineReducers } from "redux";
+// import { Provider } from "react-redux";
+// import token from "./reducer/token";
+// import currentReservation from "./reducer/currentReservation";
+// const store = createStore(combineReducers({ token, currentReservation }));
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/"
-            exact
-            component={Home}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/games"
-            component={MainPage}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/news"
-            component={NewsPage}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/reservation/overview"
-            component={OverviewPage}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/reservation/confirmation"
-            component={ConfirmationPage}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/reservation"
-            component={ReservationPage}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/profile"
-            component={ProfilePage}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/signup"
-            component={CreateAccount}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/signin"
-            component={SignInPage}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/club/signup"
-            component={CreateClubAccount}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/club/signin"
-            component={ClubSignIn}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/club/dashboard"
-            component={Dashboard}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/club/calendar/edit"
-            component={EditCalendar}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/club/calendar"
-            component={CalendarScreen}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/club/profile"
-            component={ClubProfile}
-          />
-          <Route
-            path="https://powerful-lake-94672.herokuapp.com/club"
-            component={ClubMain}
-          />
-          <Route path="*" component={Home} />
-        </Switch>
-      </Router>
-    </Provider>
+    // <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/games" component={MainPage} />
+        <Route path="/news" component={NewsPage} />
+        <Route path="/reservation/overview" component={OverviewPage} />
+        <Route path="/reservation/confirmation" component={ConfirmationPage} />
+        <Route path="/reservation" component={ReservationPage} />
+        <Route path="/profile" component={ProfilePage} />
+        <Route path="/signup" component={CreateAccount} />
+        <Route path="/signin" component={SignInPage} />
+        <Route path="/club/signup" component={CreateClubAccount} />
+        <Route path="/club/signin" component={ClubSignIn} />
+        <Route path="/club/dashboard" component={Dashboard} />
+        <Route path="/club/calendar/edit" component={EditCalendar} />
+        <Route path="/club/calendar" component={CalendarScreen} />
+        <Route path="/club/profile" component={ClubProfile} />
+        <Route path="/club" component={ClubMain} />
+        <Route path="*" component={Home} />
+      </Switch>
+    </Router>
+    // </Provider>
   );
 }
 
